eabc0f04aa91f24cc4620cfcb3e84b8c8dd451bd
Tatooms <60689403+Tatooms@users.noreply.github.com>
2024-12-17 22:00:06 +0100

    Add files via upload

---
+++ data/Mangueiraxeral_7.js
@@ -0,0 +1,8 @@
+var json_Mangueiraxeral_7 = {
+"type": "FeatureCollection",
+"name": "Mangueiraxeral_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nome": "Tubería xeral 63mm", "Lonxitude": null, "Imaxe": "C:\\Users\\Dir Edu Lou\\Desktop\\Ricardo\\Traida Auga As Rociñas\\fotos\\Mangueira xeral_1.jpg" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -8.680455292914386, 42.612170066931505 ], [ -8.680453072890854, 42.612168004022031 ], [ -8.680387300706764, 42.612122371644162 ], [ -8.680309283979767, 42.612064723075548 ], [ -8.680220675502483, 42.612000020967685 ], [ -8.680166660769473, 42.611952355417621 ], [ -8.680166660769473, 42.611952355417621 ], [ -8.680165312262568, 42.611951269523978 ] ] ] } }
+]
+}
